ad38f609cb81b7c662383c8685543ee3ec31caca
hammer8130 <154071436+hammer8130@users.noreply.github.com>
2024-06-04 14:16:13 +0900

    6/4
    6/4

--- javascript수업/14-async-and-callback.js
+++ javascript수업/14-async-and-callback.js
@@ -1,19 +1,19 @@
 // callback 처리
-function logicA(){
+function logicA() {
     console.log("begin logic A")
 
     // 비동기
-    setTimeout(()=> {
+    setTimeout(() => {
         console.log("\tcallbackA called")
         console.log("\tbegin logic B")
 
         // 비동기
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log("\t\tcallbackB Called")
-        },2000)
+        }, 2000)
 
         console.log("\tend logic B")
-    },2000)
+    }, 2000)
 
     console.log("end logic A")
 }
@@ -21,41 +21,41 @@ function logicA(){
 
 // 비동기처리를 동기방식처럼 호출하기 위해 마련된 객체 -> Promise
 // Pending, Fullfill, Reject
-function logicPromise(){
-    
-    return new Promise((resolve,reject) => {
+function logicPromise() {
+
+    return new Promise((resolve, reject) => {
         // resolve 함수 -> fullfill 상태로 이전시키는 역할
         // reject 함수 -> reject 상태로 이전시키는 역할
         console.log("begin logicPromise")
         // 비동기
-        setTimeout(()=>{
+        setTimeout(() => {
             // 내부 로직 수행
             resolve("RESOLVE")
             // reject("REJECTED")
-        },3000)
+        }, 3000)
 
         console.log("end logicPromise")
     })
 }
 // logicPromise()
 
-function testLogicPromise(){
+function testLogicPromise() {
     console.log("Test Logic Promise")
 
     logicPromise()  // Promise를 반환하는 함수(혹은 객체)
-    .then(value =>{ // Promise 내부에서 resolve 함수로 전달해 준 값 (fullfill 상태로 전달)
-        console.log("PROMISE resolve:",value)
-    })
-    .catch(reason => { // Promise 내부에서 reject 함수로 전달해 준 값 (rejected 상태로)
-        console.log("PROMISE reject:",reason)
-    })
-}  
+        .then(value => { // Promise 내부에서 resolve 함수로 전달해 준 값 (fullfill 상태로 전달)
+            console.log("PROMISE resolve:", value)
+        })
+        .catch(reason => { // Promise 내부에서 reject 함수로 전달해 준 값 (rejected 상태로)
+            console.log("PROMISE reject:", reason)
+        })
+}
 // testLogicPromise() 
 
 // async / await
 // -> Promise를 손쉽게 사용하기 위한 키워드
 // async 함수는 비동기 처리를 위해 Promise를 생성한다.
-async function f(){
+async function f() {
     console.log("async function")
     return 1
 }
@@ -64,15 +64,15 @@ async function f(){
 
 // 가상 시나리오
 // 네트워크 통신을 통해 데이터를 가져오는 과정  
-function fetchData(){
+function fetchData() {
     // return new Promise ((resolve, reject) => {
     //     setTimeout(() => {
     //         resolve("서버로부터 가져온 데이터")
     //     },2000)
     // })
 
-    return new Promise ((resolve,reject)=>{
-        setTimeout(() =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             resolve("서버로부터 가져온 데이터")
         })
     })
@@ -81,15 +81,15 @@ function fetchData(){
 
 // async는 promise를 만들어내는 키워드
 // 함수 앞에 async를 명시해줘야 그 자체를 하나의 promise로 사용 가능
-async function processData(){
+async function processData() {
     console.log("데이터를 가져오는 중...")
     try {
         // await : Promise가 해결될 때까지 코드를 일시 중지시킴
         //          Promise 해소되면 Promise의 결과값을 반환
         // await 키워드 함수 사용을 위해 반드시 함수에 async가 붙어있어야 함.
         const data = await fetchData()
-        console.log("가져온 데이터:",data)
-    }catch(error){
+        console.log("가져온 데이터:", data)
+    } catch (error) {
         console.error(error)
     }
 }
